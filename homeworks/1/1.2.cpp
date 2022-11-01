2ba3e583b2ef6303a96f7e56c28536aef4bef6fe
Giuseppe <capassog97@gmail.com>
2022-11-01 19:27:02 +0100

    Passato a asoluzione con dizionario

--- homeworks/1/1.2.cpp
+++ homeworks/1/1.2.cpp
@@ -1,83 +1,95 @@
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h>
+#include <map>
+#include <algorithm>
+#include <cassert>
 
-using namespace std;
 #define DEBUG 1
 
-int* solution(const vector<int>& input) {
-    vector<pair<int, int>> appoggio;
+void insert_in_map(std::map<int, std::vector<int>>& m, const int key, const int pos) {
+    const auto it = m.find(key);
 
-    int* results = new int[input.size()];
+    // elemento trovato
+    if (it != m.end()) {
+        it->second.push_back(pos);
+    } else {
+        // se non trovo l'elemento creo un nuovo vettore e lo associo alla chiave
+        std::vector<int> v;
+        v.push_back(pos);
+        m.insert({key,  v});
+    }
+    
+}
 
+const std::vector<int> find_min(std::vector<int>& input) {
+    std::map<int, std::vector<int>> map;
+    std::vector<int> result (input.size());
+
+    // popolo la mappa
     for (int i = 0; i < input.size(); ++i) {
-        appoggio.push_back({
-            i, input[i]
-            });
+        insert_in_map(map, input[i], i);
     }
+    // ordino il vettore con std::stable_sort O(nlog(n))
+    std::stable_sort(input.begin(), input.end());
 
-    stable_sort(appoggio.begin(), appoggio.end(), [](pair<int, int> first, pair<int, int> second) {
-        return first.second < second.second;
-        });
-
-    int index, value, tmp, offset;
-    for (int i = 0; i < appoggio.size(); ++i) {
-        value = appoggio[i].second;
-
-        if (appoggio.size() - 1 != i) {
-            offset = 0;
-            tmp = appoggio[i + 1].second;
-
-            while (value == tmp) {
-                if (tmp == value) {
-                    offset += 1;
-                    tmp = appoggio[i + offset + 1].second;
-                }
-            }
+    std::vector<int> positions;
+    // scorro il vettore ordinato e popolo il vettore dei risultati
+    for (int i = 0; i < input.size() ; ++i) {
+        positions = map[input[i]];
+        for (const int position: positions) {
+            result[position] = i + positions.size() -1;
         }
+        i+=positions.size() -1 ;
+    } 
 
-        // 4 5 5 6 7 9
-        // 5 6 4 5 7 9
-        if (offset >= 1) {
-            for (int j = 0; j < offset + 1; ++j) {
-                index = appoggio[i + j].first;
-                results[index] = offset + i;
-            }
+    return result;
+}
 
-        }
-        else {
-            index = appoggio[i + offset].first;
-            results[index] = offset + i;
-
-        }
-        i += offset;
-
+std::ostream& print_vector(const std::vector<int>& input, std::ostream& out) {
+    for (const int elem: input) {
+        out << elem << ' ';
     }
-    return results;
+    return out;
+}
+
+void assert_vector(const std::vector<int> a, const std::vector<int> b) {
+    assert(a.size() == b.size());
+
+    for (int i = 0; i < a.size(); ++i) {
+        assert(a[i] == b[i]);
+    }
 }
 
 int main() {
+    // table testing: 
+    // input di test
+    std::vector<std::vector<int>> inputs = {
+        { 5, 6, 4, 5, 7, 9 }, 
+        { 6, 5, 9, 11, 25, 19 }, 
+        { 2, 4, 3, 6, 6, 9, 3 },
+        { 1, 1, 1, 1, 1, 1, 1 },
+    };
+    // valori attesi
+    std::vector<std::vector<int>> results = {
+        { 2, 3, 0, 2, 4, 5 }, 
+        { 1, 0, 2, 3, 5, 4 },
+        { 0, 3, 2, 5, 5, 6, 2 },
+        { 6, 6, 6, 6, 6, 6, 6 }
+    };
+    std::vector<int> result;
 
-    vector<int> input = { 5, 6, 4, 5, 7, 9 };
-    int* results = solution(input);
+    for (int i = 0; i < inputs.size(); ++i) {
+        if (DEBUG) {
+            std::cout<<"INPUT : "; print_vector(inputs[i], std::cout) << std::endl;
+        }
 
-    // for (int i = 0; i < input.size(); ++i) {
-    //     cout<<results[i]<<endl;
-    // }
-    // cout<<endl;
+        result = find_min(inputs[i]);
+        
+        if (DEBUG) {
+            std::cout<<"OUTPUT: "; print_vector(result, std::cout) << "\n\n";
+        }
 
-    // delete []results;
-
-    input = { -6,3,2,4,6,-5,8 };
-
-
-    // 0 3 2 4 5 1 6
-    results = solution(input);
-
-    for (int i = 0; i < input.size(); ++i) {
-        cout << results[i] << endl;
+        assert_vector(result, results[i]);
     }
-    cout << endl;
 
-    delete[]results;
 }
