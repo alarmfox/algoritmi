8e4296d956b2918956b4545b27ee32bc19cf1db9
Giuseppe <capassog97@gmail.com>
2022-11-19 00:36:22 +0100

    Completato esercizio 2.1 Co-authored-by: AndreaDino <AndreaDino@users.noreply.github.com>

---
+++ homeworks/2/2.1.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+typedef std::vector<std::vector<int>> Matrix;
+
+#define DEBUG 0
+
+void print_matrix(const Matrix &m) {
+  for (int i = 0; i<m.size(); ++i) {
+    for (int j = 0; j < m[i].size(); ++j) {
+      std::cout<<m[i][j];
+    }
+    std::cout<<std::endl;
+  }
+}
+
+bool is_in(const int start, const int end, const int value) {
+  return value <= end && value >= start;
+}
+
+bool is_valid(const Matrix& m, int x, int y, const Matrix& visited) {
+  const int rows = m.size(), columns = m[0].size();
+
+  if (!is_in(0, rows -1, x) || !is_in(0, columns-1, y)) {
+    return false;
+  }
+
+  if (visited[x][y] == 1) {
+    return false;
+  }
+
+  if (
+    (is_in(0, rows-1, x+1) && m[x+1][y] == 0) || 
+    (is_in(0, rows-1, x-1) && m[x-1][y] == 0) || 
+    (is_in(0, columns-1, y+1) && m[x][y+1] == 0) ||
+    (is_in(0, columns-1, y-1) && m[x][y-1] == 0)
+    ) {
+    return false;
+  }
+
+  return true;
+}
+
+void find_min_path(const Matrix& m, Matrix & visited, int start_x, int start_y, int end_x, int end_y, int &res, int &final_length) {
+  if (start_x == end_x && start_y == end_y ) {
+    if (res < final_length) {
+      final_length = res;
+
+    }
+    return;
+  }
+
+  visited[start_x][start_y] = 1;
+  res++;
+
+  if (DEBUG) {
+    print_matrix(visited);
+    std::cout<<std::endl;
+  }
+
+  // andiamo sotto
+  if (is_valid(m, start_x+1, start_y, visited)) {
+    find_min_path(m, visited, start_x+1, start_y, end_x, end_y, res, final_length);
+  } 
+
+  // andiamo a destra
+  if (is_valid(m, start_x, start_y+1, visited)) {
+    find_min_path(m, visited, start_x, start_y+1, end_x, end_y, res, final_length);
+  } 
+
+  // andiamo sopra
+  if (is_valid(m, start_x-1, start_y, visited)) {
+    find_min_path(m, visited, start_x-1, start_y, end_x, end_y, res, final_length);
+  } 
+
+  // andiamo a sinistra
+  if (is_valid(m, start_x, start_y-1, visited)) {
+    find_min_path(m, visited, start_x, start_y-1, end_x, end_y, res, final_length);
+  } 
+
+  visited[start_x][start_y] = 0;
+
+  res--;
+  
+}
+
+void solution(const Matrix& m) {
+  const int last_column = m[0].size() -1;
+  int start_x, start_y, end_x, end_y, res = 0, final_length = INT_MAX;
+  
+  for (int i = 0; i < m.size(); ++i) {
+    start_x = i;
+    start_y = 0;
+    for (int j = 0; j < m.size(); j++) {
+      Matrix visited (m.size(), std::vector<int>(m[0].size()));
+      end_x = j;
+      end_y = last_column;
+      res = 0;
+      final_length = INT_MAX;
+      find_min_path(m, visited, start_x, start_y, end_x, end_y, res, final_length); 
+      std::cout 
+      << "start_x: " << start_x << " "
+      << "start_y: " << start_y << " "
+      << "end_x: " << end_x << " "
+      << "end_y: " << end_y << " "
+      << "result: ";
+      if (final_length == INT_MAX) {
+        std::cout<<"no result";
+      } else {
+        std::cout<< final_length;
+      }
+
+      std::cout<<std::endl;
+
+    }
+  }
+
+}
+
+int main() {
+  Matrix m; 
+  m.push_back({1,1,1,1,0,1,1});
+  m.push_back({1,1,1,1,1,1,1});
+  m.push_back({0,1,1,1,1,1,1});
+  m.push_back({1,1,1,1,1,1,1});
+  m.push_back({1,1,1,0,1,1,1});
+  m.push_back({1,1,1,1,0,1,1});
+  solution(m);
+
+}
