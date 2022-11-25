95660d29208c06c76aa8edcd13201744cf9990c5
Giuseppe <capassog97@gmail.com>
2022-11-25 22:28:57 +0100

    Aggiunto esercizio 2.2

---
+++ homeworks/2/2.2.cpp
@@ -0,0 +1,19 @@
+// Number of ways in which participant can take part.
+#include<iostream>
+ 
+const int solution(const int x) {
+    if (x==0 || x==1) {
+      return 1;
+    }
+ 
+    else {
+        return solution(x-1) + (x-1)*solution(x-2);
+    }
+               
+}
+ 
+int main() {
+    int x = 3;
+    std::cout << solution(x) << std::endl;
+    return 0;
+}
