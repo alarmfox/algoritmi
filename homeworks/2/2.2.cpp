0fc5e9a28ef229aa16f65aae2c249607512b5bdf
Giuseppe <capassog97@gmail.com>
2022-11-25 22:53:13 +0100

    Refactoring

--- homeworks/2/2.2.cpp
+++ homeworks/2/2.2.cpp
@@ -1,22 +1,20 @@
 #include<iostream>
 #include <vector>
 
-const int solution(const int x) {
-    int pairs[x+1];
+const int solution(const int n) {
+    int pairs[n+1];
     pairs[0] = pairs[1] = 1;
  
-    for (int i=2; i<=x; i++)
+    for (int i=2; i<=n; i++)
        pairs[i] = pairs[i-1] + (i-1)*pairs[i-2];
  
-    return pairs[x];
+    return pairs[n];
                
 }
  
 int main() {
-    std::vector<int> inputs = {3, 4, 5};
+    const std::vector<int> inputs = {3, 4, 5};
     for (const int input : inputs) {
       std::cout << solution(input) << std::endl;
     }
-
-
 }
