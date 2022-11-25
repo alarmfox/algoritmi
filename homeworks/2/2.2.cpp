719171eab5a9048b06b8a64a9216f79fd3358349
Giuseppe <capassog97@gmail.com>
2022-11-25 22:34:24 +0100

    Passato a dynamic programming

--- homeworks/2/2.2.cpp
+++ homeworks/2/2.2.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
- 
+#include <vector>
+
 const int solution(const int x) {
-    if (x==0 || x==1) {
-      return 1;
-    } else {
-        return solution(x-1) + (x-1)*solution(x-2);
-    }
+    int pairs[x+1];
+    pairs[0] = pairs[1] = 1;
+ 
+    for (int i=2; i<=x; i++)
+       pairs[i] = pairs[i-1] + (i-1)*pairs[i-2];
+ 
+    return pairs[x];
                
 }
  
 int main() {
-    int x = 3;
-    std::cout << solution(x) << std::endl;
-    return 0;
+    std::vector<int> inputs = {3, 4, 5};
+    for (const int input : inputs) {
+      std::cout << solution(input) << std::endl;
+    }
+
+
 }
