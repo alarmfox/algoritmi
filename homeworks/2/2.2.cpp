984e1182e2970a69def072f5a02a2882d1421e85
Giuseppe <capassog97@gmail.com>
2022-11-25 22:59:17 +0100

    Aggiunte parentesi

--- homeworks/2/2.2.cpp
+++ homeworks/2/2.2.cpp
@@ -5,8 +5,9 @@ const int solution(const int n) {
     int pairs[n+1];
     pairs[0] = pairs[1] = 1;
  
-    for (int i=2; i<=n; i++)
+    for (int i=2; i<=n; i++) {
        pairs[i] = pairs[i-1] + (i-1)*pairs[i-2];
+    }
  
     return pairs[n];
                
