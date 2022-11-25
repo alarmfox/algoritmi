71e66ef934d9f658d8fd491d6e31ddf31c4cb3c9
Giuseppe <capassog97@gmail.com>
2022-11-25 22:29:35 +0100

    Refactoring

--- homeworks/2/2.2.cpp
+++ homeworks/2/2.2.cpp
@@ -4,9 +4,7 @@
 const int solution(const int x) {
     if (x==0 || x==1) {
       return 1;
-    }
- 
-    else {
+    } else {
         return solution(x-1) + (x-1)*solution(x-2);
     }
                
