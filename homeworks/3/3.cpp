b4591c3a7ae6847f0771947b802d76a3024f921c
Giuseppe <capassog97@gmail.com>
2022-12-16 21:08:12 +0100

    Esercizio 3
    Co-authored-by: AndreaDino <AndreaDino@users.noreply.github.com>

---
+++ homeworks/3/3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include <vector>
+#include <cassert>
+
+#define DEBUG 1
+int solution(const std::string &s, const std::string& rev_s) {
+    int m = s.length();
+    int n = rev_s.length();
+ 
+    int dp[m+1][n+1];
+ 
+    for (int i=0; i<=m; i++)
+    {
+        for (int j=0; j<=n; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (s[i-1] == rev_s[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = std::max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+ 
+    return dp[m][n];
+}
+ 
+int main() {
+    
+    std::vector<std::pair<std::string, int>> inputs = {
+        {"madam", 5},
+        {"sulkjhgfas", 3},
+        {"xqcow", 1}
+    };
+
+    int res;
+    for (const auto input: inputs) {
+      std::string rev(input.first);
+      std::reverse(rev.begin(), rev.end());
+      res = solution(input.first, rev);
+      assert(res == input.second);
+      if (DEBUG) {
+        std::cout<<"input: "<<input.first<< " valore atteso: " << input.second << " risultato: "  << res << std::endl;
+      }
+    }
+    return 0;
+}
+
+
+
+
+
