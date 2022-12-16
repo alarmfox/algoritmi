29cee3729bb17e87f8f413b539adb8aaa435a1ab
Giuseppe <capassog97@gmail.com>
2022-12-16 21:41:37 +0100

    Inizializzata prima riga e prima colonna a 0
    Co-authored-by: AndreaDino <AndreaDino@users.noreply.github.com>

--- homeworks/3/3.cpp
+++ homeworks/3/3.cpp
@@ -10,17 +10,23 @@ int solution(const std::string &s, const std::string& rev_s) {
     int n = rev_s.length();
  
     int dp[m+1][n+1];
+
+    for (int i = 0; i < m; ++i) {
+      dp[0][i] = 0;
+    }
  
-    for (int i=0; i<=m; i++)
-    {
-        for (int j=0; j<=n; j++)
-        {
-            if (i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if (s[i-1] == rev_s[j-1])
-                dp[i][j] = dp[i-1][j-1] + 1;
-            else
+    for (int i = 0; i < m; ++i) {
+      dp[i][0] = 0;
+    }
+
+    for (int i=1; i<=m; i++) {
+        for (int j=1; j<=n; j++) {
+            if (s[i-1] != rev_s[j-1]) {
                 dp[i][j] = std::max(dp[i-1][j], dp[i][j-1]);
+            }
+            else {
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
         }
     }
  
@@ -47,8 +53,3 @@ int main() {
     }
     return 0;
 }
-
-
-
-
-
