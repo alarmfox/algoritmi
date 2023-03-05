0bf7e8fd95bf70b6d8dabe92923c3555eb3ab6d8
Giuseppe <capassog97@gmail.com>
2023-03-05 12:57:35 +0100

    Aggiunto template esame

---
+++ template-esame/main.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+#include <istream>
+
+#define TEST 0
+int main() {
+
+  std::istream *in = &std::cin;
+
+  if (TEST) {
+    in = new std::ifstream("input.txt");
+    if (in->fail()) {
+      std::cout << "errore" << std::endl;
+      return 2;
+    }
+  }
+
+  std::istream &cin = *in;
+  
+
+  // CODICE
+
+  if (TEST)
+    delete in;
+}
